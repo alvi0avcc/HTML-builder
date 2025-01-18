7a127c3de93013b0b600db82d4f3b4fef2cfa0d7
Aleksandr Vavilov <alvi.ua@gmail.com>
2025-01-18 12:16:57 +0300

    feat: 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'text.txt');
+
+function out(...str){
+    process.stdout.write(str.join(''));
+}
+
+let readAll = "";
+
+const readableStream = fs.createReadStream(filePath, 'utf8');
+
+readableStream.on("data", (readChunk) => {
+    readAll += readChunk;
+});
+
+readableStream.on("end", () => out(readAll));
